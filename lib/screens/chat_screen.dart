4554b3d4e5942c10507c4965250f6e495ea5ee2d
8ctopotamus <joshnaylor88@gmail.com>
2020-01-05 18:10:33 -0600

    Query example + Streams example

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,6 +20,7 @@ class _ChatScreenState extends State<ChatScreen> {
   void initState() {
     super.initState();
     getCurrentUser();
+
   }
 
   void getCurrentUser() async {
@@ -34,6 +35,21 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+//  Future<QuerySnapshot> getMessages () async {
+//    final messages = await _firestore.collection('messages').getDocuments();
+//    for (var message in messages.documents) {
+//      print(message.data);
+//    }
+//  }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.documents) {
+        print(message.data);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,8 +59,10 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
+                messagesStream();
+//                getMessages();
+//                _auth.signOut();
+//                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
